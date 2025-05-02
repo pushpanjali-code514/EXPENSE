eb5aa24cf805a53eeb6a3b8fea984dcd6def338d
pushpanjali4444 <pushpanjalibhagat539@gmail.com>
2025-05-02 21:33:46 +0530

    files added

--- components/header.jsx
+++ components/header.jsx
@@ -14,7 +14,7 @@ const Header = async () => {
         <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
             <Link href="/">
                 <Image
-                src={"/1000043824-removebg-preview.png"}
+                src={"/Risizelogo.png"}
                 alt="Logo"
                 height={200}
                 width={1000}
